60ce949bc047a861e6a7601fe93d7bcb66fab74f
Kelvin Lin <kelvin@bu.edu>
2023-10-03 15:45:08 -0400

    Added toasts and conditionals when username and password are correct/incorrect

--- app/src/main/java/com/example/flashcardapplication/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.flashcardapplication
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,19 +15,34 @@ import com.example.flashcardapplication.ui.theme.FlashcardApplicationTheme
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val bSubmit = findViewById<Button>(R.id.submit_button)
-        val login  = findViewById<EditText>(R.id.login)
-        val password = findViewById<EditText>(R.id.password)
+        val submit_button = findViewById<Button>(R.id.submit_button)
+        val login_field  = findViewById<EditText>(R.id.login)
+        val password_field = findViewById<EditText>(R.id.password)
+        submit_button.setOnClickListener{
+            var login_input = login_field.text.toString()
+            var password_input = password_field.text.toString()
+            if ((login_input != "Usernamez")||(password_input != "Password3")) {
+               var incorrect_toast = Toast.makeText(this,"Your Username/Password is Wrong!", Toast.LENGTH_SHORT)
+                incorrect_toast.show()
+            }
+            else{
+                var correct_toast = Toast.makeText(this,"Login Successful!" , Toast.LENGTH_SHORT)
+                correct_toast.show()
+            }
+            }
+
+        }
 
 
     }
-}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
